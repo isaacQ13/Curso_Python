8035690e50c7e3993460b0142b50bd3f56a9aaca
isaacQ13 <isaac.quinto@ufpe.br>
2021-08-21 19:38:17 -0300

    Atualizações das Aulas

---
+++ Aula 004/Exercicio Aula 004.py
@@ -0,0 +1,11 @@
+"""
+String -> Seu nome
+Int -> Sua idade
+Float -> Sua altura
+Bool -> É maior de idade?
+"""
+
+print("Isaac Quinto", type("Isaac Quinto"))
+print(20, type(20))
+print(1.87, type(1.87))
+print(20>=18, type(20>=18))
