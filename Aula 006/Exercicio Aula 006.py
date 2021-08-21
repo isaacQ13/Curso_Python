89695afaf858ef06b8db73bb27238247d3061a2c
isaacQ13 <isaac.quinto@ufpe.br>
2021-08-21 19:59:57 -0300

    Atualização da aula 6 e Exercicios + Adição da Aula 7

---
+++ Aula 006/Exercicio Aula 006.py
@@ -0,0 +1,10 @@
+"""
+Descrever Caracteristicas suas com python e seu IMC
+"""
+nome = "Isaac Bastos"
+idade = 20
+altura = 1.87
+peso = 89
+imc = peso / (altura * altura)
+
+print(nome, "tem", idade, "Anos de idade e seu IMC é", imc)
